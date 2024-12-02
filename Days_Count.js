b5059af78efc95adbc24b2c0509f3ee9a1ddf792
Aman Modanwal <112753840+amanmodanwal28@users.noreply.github.com>
2024-12-03 00:11:55 +0530

    Update Days_Count.js

--- Days_Count.js
+++ Days_Count.js
@@ -1,8 +1,10 @@
-const  counting = 40;
+const  counting = 40;;
 const array = [1,2,3,4,5,6,7,8,8,9];
 for(let i = 1;i<= counting ; i++){
   console.log(` the total day commit in your repo is  ${i}`);
 };
+
+
 
 array.forEach((num) => {
   console.log(`${num}`);
