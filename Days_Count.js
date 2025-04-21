6ef17d5a6161bd320949765b4d42c17e762230b5
Aman Modanwal <112753840+amanmodanwal28@users.noreply.github.com>
2025-04-21 15:56:27 +0530

    Update Days_Count.js

--- Days_Count.js
+++ Days_Count.js
@@ -8,3 +8,10 @@ array.forEach((num) => {
 })
 
 https://chatgpt.com/share/67ff283c-203c-800c-9789-ccfb4094e922
+
+[21/04, 3:51 pm] Garv: Ensured timely availability of materials by monitoring stock levels and coordinating with procurement team and china team.
+
+Maintained accurate inventory records using ERP systems to support production planning and reduce material shortages.
+
+Coordinated with production, warehouse, and purchasing teams for smooth material flow across departments.
+[21/04, 3:54 pm] Garv: Releasing manufacturing order( MO) for production as per the given plan.
